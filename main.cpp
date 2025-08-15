1b3ef98ada32d660cd9abcc87c52b058f4c5eaa6
Herb Segismar <herbert.segismar@yahoo.com>
2025-08-16 10:14:38 +1200

    makefile

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML work!");
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
